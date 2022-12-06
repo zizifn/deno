7b615e0feed88ddac0a4e04167eb82b6eb41bd3a
zizfn <1803942+zizifn@users.noreply.github.com>
2022-12-06 20:26:04 +0800

    add socketio

---
+++ app/man.ts
@@ -0,0 +1,18 @@
+import { serve } from "https://deno.land/std@0.166.0/http/server.ts";
+import { Server } from "https://deno.land/x/socket_io@0.2.0/mod.ts";
+
+const io = new Server();
+
+io.on("connection", (socket) => {
+  console.log(`socket ${socket.id} connected`);
+
+  socket.emit("hello", "world");
+
+  socket.on("disconnect", (reason) => {
+    console.log(`socket ${socket.id} disconnected due to ${reason}`);
+  });
+});
+
+await serve(io.handler(), {
+  port: 3000,
+});
